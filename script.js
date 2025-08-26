5d7d90113744f2af79e496e638679d93cce183fe
Imtiaz AL Kabir <imtiazalkabir@gmail.com>
2025-08-26 22:45:01 +0600

    points and heart count functionalitis done

--- script.js
+++ script.js
@@ -8,3 +8,47 @@ for (const cardHeart of cardsHeart) {
     document.getElementById("nav-heart").innerText = heartCount;
   });
 }
+
+const cardCallButtons = document.getElementsByClassName("card-call");
+for (const cardCallButton of cardCallButtons) {
+  cardCallButton.addEventListener("click", () => {
+    const subTitle = cardCallButton.parentNode.parentNode.children[2].innerText;
+    const phnNumber =
+      cardCallButton.parentNode.parentNode.children[3].innerText;
+
+    let pointCount = document.getElementById("points-count").innerText;
+    let newPoints = Number(pointCount) - 20;
+    if (pointCount >= 20) {
+      alert(`Calling ${subTitle} ${phnNumber}....`);
+      document.getElementById("points-count").innerText = newPoints;
+      let date = new Date().toLocaleTimeString();
+      const history = document.getElementById("history-container");
+      const newDiv = document.createElement("div");
+      newDiv.innerHTML = `
+     <div
+                class="flex justify-between items-center bg-[#fafafa] rounded-lg p-4 mt-4"
+              >
+                <div>
+                  <h1 class="font-bold">${subTitle}</h1>
+                  <p class="font-semibold">${phnNumber}</p>
+                </div>
+                <div>${date}</div>
+              </div>
+    
+    `;
+
+      history.append(newDiv);
+    } else {
+      alert(` You Dont Have Enought points to Call`);
+    }
+  });
+}
+
+const clear = document
+  .getElementById("clear-btn")
+  .addEventListener("click", () => {
+    let history = document.getElementById("history-container");
+    history.innerHTML = "";
+  });
+
+  
