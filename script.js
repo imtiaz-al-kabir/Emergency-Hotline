b2b7067f6e3e6d9d25c920f46482affc0ee9d38c
Imtiaz AL Kabir <imtiazalkabir@gmail.com>
2025-08-26 21:35:00 +0600

    card section done functionalities in progress

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-console.log("connected");
+const cardsHeart = document.getElementsByClassName("card-heart");
+console.log(cardsHeart);
+for (const cardHeart of cardsHeart) {
+  cardHeart.addEventListener("click", () => {
+    const navHeart = document.getElementById("nav-heart").innerText;
+    let heartCount = Number(navHeart) + 1;
+    console.log(heartCount);
+    document.getElementById("nav-heart").innerText = heartCount;
+  });
+}
