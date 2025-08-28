5afe7763b9f918957585ca1233a6febf023ac48f
Imtiaz AL Kabir <imtiazalkabir@gmail.com>
2025-08-28 15:55:43 +0600

    mobile version corrections

--- script.js
+++ script.js
@@ -4,7 +4,6 @@ for (const cardHeart of cardsHeart) {
   cardHeart.addEventListener("click", () => {
     const navHeart = document.getElementById("nav-heart").innerText;
     let heartCount = Number(navHeart) + 1;
-    console.log(heartCount);
     document.getElementById("nav-heart").innerText = heartCount;
   });
 }
